63dc9890b695c61a9a80866c0d54159fa1b98259
gustavwalls <paredes.ga0610@gmail.com>
2024-10-16 00:22:04 -0500

    avanzando configuraciones

--- src/modules/home/components/Header/Header.tsx
+++ src/modules/home/components/Header/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./Header.scss";
+//import logoRimac from '../../../../assets/Familia_rimac.png'
 export default function Header() {
   return (
     <header className='header container'>
         <div className='header__container'>
-            <span>logo</span>
+            <div>
+           <img src="" alt="" />
+            </div>
             <div className='header__contact'>
                 <p className='header__text'>¡Compra por este medio!</p>
                 <p className='header__number'>(01) 411 6001</p>
